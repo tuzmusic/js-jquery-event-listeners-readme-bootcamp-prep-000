2bd454d9a27b6ef57de77353b7995a8020b298b3
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-04 22:58:17 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
 
 $(document).ready(function(){
-
-// call functions here
+  // mocha.run()
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 
 });
+
+function getIt() {
+  $('p').on('clear click', (event) => {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', () => {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', (key) => {
+    console.log(key.key)
+    if (key.which === 71) {
+      alert('You pressed the G key.')
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', () => {
+      alert('Your form is going to be submitted now.')
+      // return
+  })
+}
